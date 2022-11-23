887bd724bb0a25db2f1136daa253eac7cc6d6c06
Le Pham <phamtle724@gmail.com>
2022-11-22 16:10:28 -0800

    square shows, scale and hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div
+      className="box"
+      drag
+      dragConstraints={{
+        top: -50,
+      left: -50,
+      right: 50,
+      bottom: 50,
+      }}
+
+      whileHover={{scale: 1.2}}
+      whileTap={{scale:0.8}}
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+
+
+      }}
+    />
   );
 }
 
